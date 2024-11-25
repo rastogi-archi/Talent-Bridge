b92249cd263fd99fbc9072247519472c3ec2fd86
rastogi-archi <rastogiarchi18@gmail.com>
2024-11-25 12:40:23 +0530

    files initialised

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/Auth.route.js"
 import postRoutes from "./routes/Post.route.js"
@@ -27,6 +28,11 @@ const __dirname = path.resolve();
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true
+}
+))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
